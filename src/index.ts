35f9f4dc3c6405335093890650767ce43cbef90c
act99 <dorxm999@gmail.com>
2021-11-09 14:28:34 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+const sayHi = (name:string, age:number, gender:string):void =>
+{
+   return console.log(`Hi. You are ${name} ${age} ${gender}`)
+}
+
+
+
+sayHi("name", 19, "gender")
+
+export { };
